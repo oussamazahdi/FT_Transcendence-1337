9dfb95e9ad21071f907ed753bf68635ebfc4a7f1
oussama <oussama.zahdi25@gmail.com>
2025-11-17 18:19:35 +0100

    change covers and change component bg colors in /game/page.tsx

--- frontend/src/app/(protected)/game/pingPong/page.tsx
+++ frontend/src/app/(protected)/game/pingPong/page.tsx
@@ -32,11 +32,11 @@ const LocalGame = ()=>{
 	  return (
 	    <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 w-full max-w-6xl'>
 		   
-		  <div key={localGameItems[0].titel} className='rounded-3xl bg-gray-400/30 px-3 pt-3 text-center'>
+		  <div key={localGameItems[0].titel} className='rounded-3xl bg-[#0F0F0F]/65 px-3 pt-3 text-center'>
 				<img src={localGameItems[0].cover} alt={localGameItems[0].alt} className='w-full h-90 object-cover rounded-xl'/>
 				<h1 className='font-bold text-2xl pt-3 pb-1'>{localGameItems[0].titel}</h1>
 				<p className='pb-3 text-[#D5D5D5]'>{localGameItems[0].description}</p> 
-				<button className=' bg-[#181818]/65 hover:bg-[#1B1B1B]/65 mb-3 py-2 w-full rounded-lg \
+				<button className=' bg-[#333333]/60 hover:bg-[#333333]/40 mb-3 py-2 w-full rounded-lg \
 				cursor-pointer shadow-md font-medium'
 				onClick={() => setIsModalVisible(true)}>
 			  {localGameItems[0].button}
