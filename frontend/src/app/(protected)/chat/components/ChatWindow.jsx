62bed6ad1b3b364b6c898c45d77c556beb8d2947
oussamaza25 <oussama.zahdi25@gmail.com>
2025-12-12 11:03:00 +0100

    remove all

--- frontend/src/app/(protected)/chat/components/ChatWindow.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ChatWindow({friend}){
-    return (
-        <div>
-            {friend.playerName}
-        </div>
-    )
-}
--- frontend/src/app/(protected)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function dashboard() {
-  return (
-    <div>
-      <h1>this is dashboard page</h1>
-    </div>
-  );
-}
