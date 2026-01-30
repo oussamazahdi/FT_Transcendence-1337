c03ffe0290a86153456d45bb85bd294a48b0337f
Kamal El Alami <kelalami123@gmail.com>
2026-01-30 01:01:42 +0100

    rate limiting fixing small issue

--- backend/config/rateLimit.config.js
+++ backend/config/rateLimit.config.js
@@ -1,4 +1,9 @@
 export const rateLimitConfig = {
+    keyGenerator: function (request) {
+        if (request.user.userId && request.ip)
+            return request.user.userId;
+        return request.ip;
+    },
     max: 100,
     timeWindow: '1 minute',
     errorResponseBuilder: () => ({
